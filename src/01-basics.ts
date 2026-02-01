876f9dfdf53e14f37952aff954564d9ea7c844a8
PaulsGitH <paullydee0@gmail.com>
2026-02-01 20:46:22 +0000

    Added type-safety to friend data.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,28 +1,37 @@
 // (Optional) Change the names below to your friends.
 
 interface Friend {
-    name: string;
-    phone: string;
-    age: number
+  name: string;
+  phone: string;
+  age: number
 }
 
+interface Colleague {
+  name: string;
+  department: string;
+  contact: {
+    email: string;
+    extension: number;
+  };
+}
 
-const friend1 = {
+const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
   age: 25,
 };
 
-const friend2 = {
+const friend2: Friend = {
   name: "Jane Costello",
   phone: "086--12345",
   age: 31,
 };
 
-const friends = [friend1, friend2];
+const friends: Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
-//   -------------------
+
+
 const colleague1 = {
   name: "Ralph Graham",
   department: "Engineering",
